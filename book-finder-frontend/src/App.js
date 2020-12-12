a12febc222adb8d5e86441dc1c802398c6ade9c5
Eduardo Borba <eduardo@jobscore.com>
2020-12-12 14:07:13 -0300

    Implement API call to backend

--- book-finder-frontend/src/App.js
+++ book-finder-frontend/src/App.js
@@ -1,8 +1,25 @@
+import React, { useState, useEffect } from "react";
 import logo from './logo.svg';
 import './App.css';
 import Button from 'react-bootstrap/Button';
+import axios from 'axios';
 
 function App() {
+  const [data, setData] = useState({ items: [] });
+  const [query, setQuery] = useState('react');
+
+  useEffect(() => {
+    let ignore = false;
+
+    async function fetchData() {
+      const result = await axios('http://localhost:3000/books?search_term=Harry Potter');
+      if (!ignore) setData(result.data);
+    }
+
+    fetchData();
+    return () => { ignore = true; }
+  }, [query]);
+
   return (
     <div className="App">
       <header className="App-header">
@@ -18,6 +35,14 @@ function App() {
         >
           Learn React
         </Button>
+        <input value={query} onChange={e => setQuery(e.target.value)} />
+        <ul>
+          {data && data.items.map(item => (
+            <li key={item.id}>
+              <a href={item.selfLink}>{item.volumeInfo.title}</a>
+            </li>
+          ))}
+        </ul>
       </header>
     </div>
   );
